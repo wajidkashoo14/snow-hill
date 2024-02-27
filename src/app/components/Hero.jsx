f25e7bb91039b6df98d51b153ae7ea09e938610d
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2024-02-27 21:09:41 +0530

    code fix

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -6,8 +6,8 @@ export default function Hero() {
   return (
     <div className={styles.container}>
       <h3 className="montserrat  text-white text-6xl font-extrabold max-[600px]:text-5xl">CRAFTING JOUERNEYS</h3>
-      <h3 className="montserrat text-white text-6xl font-extrabold max-[600px]:text-5xl">NOT JUST TRIPS</h3>
-      <button onClick={() => router.push('/contact')}>Get in touch</button>
+      <h3 className="montserrat text-white text-6xl font-extrabold max-[600px]:text-5xl ">NOT JUST TRIPS</h3>
+      <button className='max-[600px]:mb-20' onClick={() => router.push('/contact')}>Get in touch</button>
     </div>
   );
 }

4162cf43bb48f0ba3b6efc311911cc1203a97469
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2024-02-28 14:38:34 +0530

    added slider

--- src/app/components/Slider.jsx
+++ src/app/components/Slider.jsx
@@ -2,7 +2,13 @@
 import Image from "next/image";
 import React, { useState, useEffect } from "react";
 
-const images = ["/slider1.jpg", "/slider2.jpg", "/slider3.jpg"];
+const images = [
+  "/slider1.jpg",
+  "/slider2.jpg",
+  "/slider3.jpg",
+  "/slider4.jpg",
+  "/slider5.jpg",
+];
 
 function Carousel() {
   const [currentImage, setCurrentImage] = useState(0);
@@ -16,8 +22,26 @@ function Carousel() {
   }, []);
 
   return (
-    <div className="relative w-full h-full">
-      {images.map((image, index) => (
+    <div className="relative w-[100vw] h-[100vh] flex justify-center items-center">
+      {images.map((img, index) => {
+        return (
+          <Image
+            src={img}
+            height={200}
+            width={1000}
+            style={{
+              width: "80vw",
+              height: "500px",
+              objectFit: "cover",
+            }}
+            alt="kaaz"
+            className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${
+              currentImage === index ? "opacity-100" : "opacity-0"
+            } `}
+          />
+        );
+      })}
+      {/* {images.map((image, index) => (
         <Image
           width={600}
           height={300}
@@ -28,7 +52,7 @@ function Carousel() {
             currentImage === index ? "opacity-100" : "opacity-0"
           }`}
         />
-      ))}
+      ))} */}
     </div>
   );
 }
